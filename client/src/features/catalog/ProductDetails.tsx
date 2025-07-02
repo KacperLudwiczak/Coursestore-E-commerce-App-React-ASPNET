4da4eda56132822ad99655592669009a6d23111d
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-07-02 11:58:59 +0200

    Styling

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
-import { Button, Divider, Table, Grid, TableBody, TableCell, TableContainer, TableRow, TextField, Typography } from "@mui/material";
+import { Button, Table, Grid, TableBody, TableCell, TableContainer, TableRow, TextField, Typography } from "@mui/material";
 import type { Product } from "../../app/models/product";
 
 export default function ProductDetails() {
@@ -27,11 +27,14 @@ export default function ProductDetails() {
   return (
     <Grid container spacing={6} maxWidth='lg' sx={{ mx: 'auto' }}>
       <Grid size={6}>
-        <img src={product?.pictureUrl} alt={product.name} style={{ width: '100%' }} />
+          <img
+          src={`https://localhost:5001${product.pictureUrl}`}
+          alt={product.name}
+          style={{ width: '100%', borderRadius: '12px' }}
+        />
       </Grid>
       <Grid size={6}>
-        <Typography variant="h3">{product.name}</Typography>
-        <Divider sx={{ mb: 2 }} />
+        <Typography variant="h3" sx={{ mb: 2 }} >{product.name}</Typography>
         <Typography variant="h4" color='secondary'>${(product.price / 100).toFixed(2)}</Typography>
         <TableContainer>
           <Table sx={{
